a1f3f650d060100ad2e931327a1ef0c1c575785d
Yashjain0007 <124593014+Yashjain0007@users.noreply.github.com>
2023-03-30 18:04:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let element = document.getElementById("output");
+let promise = new Promise(function(resolve,reject){
+	setTimeout(
+		resolve("Hello, world!")
+		,1000)
+})
+promise.then((val)=>{
+	element.value = val;
+})
+.catch("error");
